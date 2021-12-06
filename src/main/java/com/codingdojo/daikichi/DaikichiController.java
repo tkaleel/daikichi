7ec8fb8ac19c22546b72de4f132576b6048b7543
theo <91856536+tkaleel@users.noreply.github.com>
2021-12-05 20:17:56 -0800

    add routes

--- src/main/java/com/codingdojo/daikichi/DaikichiController.java
+++ src/main/java/com/codingdojo/daikichi/DaikichiController.java
@@ -3,6 +3,7 @@ package com.codingdojo.daikichi;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/daikichi")
@@ -21,5 +22,19 @@ public class DaikichiController {
 	@RequestMapping(value="tomorrow", method=RequestMethod.GET)
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{location}")
+	public String travel(@PathVariable("location") String location){
+		return "Congratulations! You will soon travel to " + location + "!";
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") Integer number) {
+		if(number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		} else {
+			return "You have enjoyed the fruits of your labor, but now is a great time to spend time with family and friends.";
+		}
 	}
 }
